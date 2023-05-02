bf178679bd28ac8d3e1448a9bb145bf3148e63ce
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2023-05-02 23:03:05 +0300

    corrected CreateDevice modal

--- src/components/modals/CreateDevice.js
+++ src/components/modals/CreateDevice.js
@@ -1,9 +1,17 @@
 import React, { useContext } from "react";
-import { Modal, Button, Form, Dropdown } from "react-bootstrap";
+import { Modal, Button, Form, Dropdown, Row, Col } from "react-bootstrap";
 import { Context } from "../../index";
 
 const CreateDevice = ({ show, onHide }) => {
   const { device } = useContext(Context);
+  const [info, setInfo] = React.useState([]);
+  const addInfo = () => {
+    setInfo([...info, { title: "", description: "", number: Date.now() }]);
+  };
+
+  const removeInfo = (number) => {
+    setInfo(info.filter((info) => info.number !== number));
+  };
 
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
@@ -15,7 +23,7 @@ const CreateDevice = ({ show, onHide }) => {
       <Modal.Body>
         <Form>
           <Dropdown className="mt-2 mb-2">
-            <Dropdown.Toggle variant="outline" id="dropdown-basic">
+            <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
               Выберите тип
               <Dropdown.Menu>
                 {device.types.map((type) => (
@@ -25,7 +33,7 @@ const CreateDevice = ({ show, onHide }) => {
             </Dropdown.Toggle>
           </Dropdown>
           <Dropdown className="mt-2 mb-2">
-            <Dropdown.Toggle variant="outline" id="dropdown-basic">
+            <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
               Выберите бренд
               <Dropdown.Menu>
                 {device.brands.map((brand) => (
@@ -49,6 +57,31 @@ const CreateDevice = ({ show, onHide }) => {
             type="file"
           />
           <hr />
+          {info.map((i) => (
+            <Row className="mt-3" key={i.number}>
+              <Col md={4}>
+                <Form.Control placeholder={"Введите название свойства"} />
+              </Col>
+              <Col md={4}>
+                <Form.Control placeholder={"Введите описание свойства"} />
+              </Col>
+              <Col md={4}>
+                <Button
+                  onClick={() => removeInfo(i.number)}
+                  variant={"outline-danger"}
+                >
+                  Удалить
+                </Button>
+              </Col>
+            </Row>
+          ))}
+          <Button
+            className="mt-3"
+            onClick={() => addInfo()}
+            variant="outline-dark"
+          >
+            Добавить новое свойство
+          </Button>
         </Form>
       </Modal.Body>
       <Modal.Footer>
